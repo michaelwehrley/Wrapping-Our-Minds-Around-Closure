5c48b65207e1bb7e47e89c14cef32f7bc5bcbb48
Michael Wehrley <526136+michaelwehrley@users.noreply.github.com>
2022-10-25 14:50:40 -0500

    set-up

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function log (arg) {
+  document.writeln(arg)
+}
+
+/*
+  You will be defining functions below.
+  The first function `identify` is defined for you
+  but the implementation is not there.
+  The goal is to write the function that will return itself 
+  and then log it using the `log` function defined above.
+  When done successfully, you will see the
+  value x (i.e., 3) on the screen
+*/
